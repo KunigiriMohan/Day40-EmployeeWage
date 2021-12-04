29216a5c74c0e9529dff631825b773492818a09f
KunigiMohan <kmohanr5@gmail.com>
2021-12-04 08:13:55 +0530

    Added : Handling optional part of email.

--- EmailValidation.js
+++ EmailValidation.js
@@ -5,7 +5,7 @@ class EmailValidation{
     }
 
     set emailId(emailId){                               //setter for setting email
-        let emailIDRegex=RegExp('^(abc)[\\.]{1}[a-z,A-Z]{3}[\\@]{1}(bridgelabz)[\\.]{1}(co)[\\.][a-z]{2}$');            //Regex for validating Email Address
+        let emailIDRegex=RegExp('^(abc)[\\.]{1}[a-z,A-Z_,+-.]{3}[\\@]{1}(bridgelabz)[\\.]{1}(co)[\\.][a-z]{2}$');            //Regex for validating Email Address
         if(emailIDRegex.test(emailId)){
             this._emailId=emailId;
         }
@@ -53,7 +53,7 @@ catch (e){
 //UC3 Ensuring "." after bridgelabz third part i.e co part
 
 try{                                                                        //try catch block to handle errors when user enter wrong emailID
-    let object4=new EmailValidation("abc.xyz@bridgelabz!co.in");                     //insted of mandatory bridgelabz part putting google to throw error
+    let object4=new EmailValidation("abc.xyz@bridgelabz.co.in");                     //insted of mandatory bridgelabz part putting google to throw error
     console.log(object4.toString());
 }
 catch (e){
@@ -63,6 +63,16 @@ catch (e){
 try{                                                                        //try catch block to handle errors when user enter wrong emailID
     let object5=new EmailValidation("abc.xyz!bridgelabz.com.in");             //insted of @ symbol inputting ! symbol to throw error 
     console.log(object5.toString());
+}
+catch (e){
+    console.log(e);
+}
+
+//UC4 Handling optional part xyz
+
+try{                                                                        //try catch block to handle errors when user enter wrong emailID
+    let object6=new EmailValidation("abc.ab+@bridgelabz.co.in");             //handling optional part 
+    console.log(object6.toString());
 }
 catch (e){
     console.log(e);
