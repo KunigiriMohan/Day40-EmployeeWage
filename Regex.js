0f5f9036edb012e9ca42b710ce2244f10f39ad6e
KunigiMohan <kmohanr5@gmail.com>
2021-12-04 08:27:15 +0530

    Added : Restricting use of Alphbet at Ending of Pincode.

--- Regex.js
+++ Regex.js
@@ -42,3 +42,12 @@ catch (e){
     console.log(e);
 }
 
+//UC3 : Restricting taking Pincode other than Numbers at Ending.
+
+try{
+    let object2=new RegexCheck("40008B");
+    console.log(object2.toString());
+}
+catch (e){
+    console.log(e);
+}
